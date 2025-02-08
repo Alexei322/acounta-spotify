bbbc19e193cc8589c9891913689179571f3a013b
Alex <alexeibradshaw12@gmail.com>
2025-02-08 23:37:04 +0000

    Added mongoose models, implemented callback for signup/signin logic

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,26 @@
 import { Router } from "express";
-
+import { User } from "../models/user.model.js"
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.send("Auth route GET")
+router.post("/callback", async (req, res) => {
+    try {
+        const { id, firstName, lastName, imageUrl } = req.body;
+
+        const user = await User.findOne({ clerkId: id })
+
+        if (!user) {
+            //signup
+            await User.create({
+                clerkId: id,
+                fullName: `${firstName} ${lastName}`,
+                imageUrl
+            })
+        }
+        res.status(200).json({ success: true })
+    } catch (error) {
+        console.log("Error in auth callback", error);
+        res.status(500).json({ message: "internal server error", error })
+    }
 })
 
 export default router;
